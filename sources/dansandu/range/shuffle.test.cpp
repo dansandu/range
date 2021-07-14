c0c5136737689ce0dd39ada13ab34aa54597d273
Daniel Sandu <49592096+dansandu@users.noreply.github.com>
2021-07-14 19:17:28 +0300

    Update shuffle test to new generator

--- sources/dansandu/range/shuffle.test.cpp
+++ sources/dansandu/range/shuffle.test.cpp
@@ -22,7 +22,7 @@ TEST_CASE("Shuffle")
     {
         const auto myIntegers = integers(0, 1, 10) | toVector();
         const auto shuffled = myIntegers | shuffle(generator);
-        const auto expected = std::vector<int>{{1, 2, 8, 3, 5, 7, 9, 4, 6, 0}};
+        const auto expected = std::vector<int>{{0, 2, 4, 6, 8, 5, 7, 9, 3, 1}};
 
         REQUIRE(expected == shuffled);
     }
