88f89a2cbf7262ecce7cebfe1e30a41d398beb5d
Daniel Sandu <49592096+dansandu@users.noreply.github.com>
2021-10-20 02:54:41 +0300

    Update test executable

--- sources/dansandu/range/executable.test.cpp
+++ sources/dansandu/range/executable.test.cpp
@@ -1,2 +1,18 @@
-#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_RUNNER
 #include "catchorg/catch/catch.hpp"
+#include "dansandu/ballotin/logging.hpp"
+
+using dansandu::ballotin::logging::Level;
+using dansandu::ballotin::logging::Logger;
+using dansandu::ballotin::logging::UnitTestsHandler;
+
+int main(const int argumentsCount, const char* const* const arguments)
+{
+    auto& logger = Logger::globalInstance();
+    logger.setLevel(Level::debug);
+    logger.addHandler("UnitTests", Level::debug, UnitTestsHandler{"unit_tests.log"});
+
+    const auto catchResult = Catch::Session().run(argumentsCount, arguments);
+
+    return catchResult;
+}
